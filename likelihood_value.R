1d04960eecc40f3654855896f7a0a2c6e586b70a
Xiaotong Liu <xiaotongliu@xiaotongdembp.home>
2020-04-12 17:59:27 +0100

    change the conditional likelihood calculation method

--- likelihood_value.R
+++ likelihood_value.R
@@ -5,70 +5,44 @@ options(digits = 3)
 
 
 likelihood <- function (b, a_b, a_B) { ##input: P(B) & P(A|B) & P(A|¬B)
-  if ( (a_B *(1-b) + a_b * b) <= 1 ) { ## probability axiom 0 <= P(A) <= 1
-    a = a_b * b + a_B * (1-b)     ## P(A) = P(A|B)*P(B) + P(A|¬B)*P(¬B)
-    #### --------------------- 
-    ### calculate marginal likelihoods
-    marginal_value = numeric(4)
-    names(marginal_value) <- c("P(A)", "P(¬A)", "P(B)", "P(¬B)")
-    marginal_value[1] = a
-    marginal_value[2] = 1-a
-    marginal_value[3] = b
-    marginal_value[4] = 1-b
-    
-    ### --------------------------------
-    ### calculate conditional likelihood and consjunctions 
-    ### notice: when p(y) = 0, p(x|y) = NaN (undefined)
-    conjunction_value = numeric(4)
-    names(conjunction_value) <- c("P(A^B)", "P(A^¬B)", "P(¬A^B)", "P(¬B^¬A)")
-    
-    
-    if (a > 0 && a < 1) {
-      
-      ### P(¬B|A) 
-      ### Math: P(¬B|A) * P(A) = P(A|¬B) * P(¬B)
-      B_a = a_B * (1-b) / a
-      ### P(B|A) 
-      ### P(B|A) * P(A) = P(A|B) * P(B)
-      b_a = a_b*b/a
-      ### P(B|¬A)
-      ## Math P(B) = P(B|A)*A + P(B|¬A)*P(¬A)
-      b_A = (b - b_a*a) / (1-a)
-      ### P(¬B|¬A) 
-      ### Math: P(¬B) = P(¬B|A) * P(A) + P(¬B|¬A) * P(¬A)
-      B_A = ( (1-b) - B_a * a ) / (1-a)
-      
-      ###  consjuctions 
-      conjunction_value[1] = a_b*b
-      conjunction_value[2] = a_B*(1-b)
-      conjunction_value[3] = b_A*(1-a)
-      conjunction_value[4] = B_A*(1-a)
-    } else if (a == 0) {
-      conjunction_value[1] = 0
-      conjunction_value[2] = 0
-      conjunction_value[3] = b ### P(¬A) = 1; Math: P(¬A^B) = P(B|¬A) * P(¬A) = P(B)
-      conjunction_value[4] = (1-b) ### P(¬A) = 1; Math: P(¬A^¬B) = P(¬B|¬A) * P(¬A) = P(¬B)
-    } else { ### a = 1
-      conjunction_value[1] = a_b*b
-      conjunction_value[2] = a_B*(1-b) ### notice: P(A) = P(A^B) + P(A^¬B) = 1 here 
-      conjunction_value[3] = 0
-      conjunction_value[4] = 0   
-    }
-    
-    
-    ### disjunctios 
-    disjunction_value = numeric(4)
-    names(disjunction_value) <- c("P(A∨B)", "P(A^∨B)", "P(¬A∨B)", "P(¬B∨¬A)")
-    
-    disjunction_value[1] = a + b - conjunction_value["P(A^B)"] ## math: P(A∨B) = P(A) + P(B) - P(A^B)
-    disjunction_value[2] = a + (1-b) - conjunction_value["P(A^¬B)"]
-    disjunction_value[3] = (1-a) + b - conjunction_value["P(¬A^B)"]
-    disjunction_value[4] = (1-a) + (1-b) - conjunction_value["P(¬B^¬A)"]
-    
-    c(marginal_value, conjunction_value,  disjunction_value )
-  }
+  a = a_b * b + a_B * (1-b)     ## P(A) = P(A|B)*P(B) + P(A|¬B)*P(¬B)
+  #### --------------------- 
+  ### calculate marginal likelihoods
+  marginal_value = numeric(4)
+  names(marginal_value) <- c("P(A)", "P(¬A)", "P(B)", "P(¬B)")
+  marginal_value[1] = a
+  marginal_value[2] = 1-a
+  marginal_value[3] = b
+  marginal_value[4] = 1-b
+  
+  ### conditional likelihoods
+  ###P(¬A|B)
+  A_b = 1 - a_b
+  ###P(¬A|¬B)
+  A_B = 1 - a_B
+  
+  ### --------------------------------
+  ### calculate conjunctions 
+  conjunction_value = numeric(4)
+  names(conjunction_value) <- c("P(A^B)", "P(A^¬B)", "P(¬A^B)", "P(¬B^¬A)")
+  
+  
+  conjunction_value[1] = a_b*b
+  conjunction_value[2] = a_B*(1-b)
+  conjunction_value[3] = A_b*b
+  conjunction_value[4] = A_B*(1-b)
+  
+  ### disjunctios 
+  disjunction_value = numeric(4)
+  names(disjunction_value) <- c("P(A∨B)", "P(A^∨B)", "P(¬A∨B)", "P(¬B∨¬A)")
+  
+  disjunction_value[1] = a + b - conjunction_value["P(A^B)"] ## math: P(A∨B) = P(A) + P(B) - P(A^B)
+  disjunction_value[2] = a + (1-b) - conjunction_value["P(A^¬B)"]
+  disjunction_value[3] = (1-a) + b - conjunction_value["P(¬A^B)"]
+  disjunction_value[4] = (1-a) + (1-b) - conjunction_value["P(¬B^¬A)"]
+  
+  c(marginal_value, conjunction_value,  disjunction_value )
 }
-
 
 b <- seq(0, 1, by = 0.01) 
 length(b)
